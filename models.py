ec75c1d35664c2e856699c13b1cb7b7572213129
Louis <yienlouis470@gmail.com>
2025-02-28 21:44:45 +0300

    models.py

--- models.py
+++ models.py
@@ -14,6 +14,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
    
 db = SQLAlchemy()
 bcrypt = Bcrypt()
