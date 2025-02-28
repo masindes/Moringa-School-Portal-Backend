047b8040326f5cc993723e2db8829dfb0e39ac0a
Louis <yienlouis470@gmail.com>
2025-02-28 21:46:04 +0300

    models.py

--- models.py
+++ models.py
@@ -15,7 +15,8 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-   
+    password_hash = db.Column(db.String(128), nullable=False)
+    
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
