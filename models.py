f7be731afd24d5687ec42057dc8bb63352a459c2
Louis <yienlouis470@gmail.com>
2025-02-28 21:55:03 +0300

    models.py

--- models.py
+++ models.py
@@ -22,7 +22,10 @@ class User(db.Model):
 
 def set_password(self, password):
         self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-        
+
+def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
