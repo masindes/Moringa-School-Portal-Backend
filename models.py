b1cfd656c21fc5c55df0e8785c889312d770fecb
Louis <yienlouis470@gmail.com>
2025-02-28 21:43:19 +0300

    models.py

--- models.py
+++ models.py
@@ -10,6 +10,11 @@ import string
 
 otp_storage ={}
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+   
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
